0e1fd217a8ead5d505ceb6a4cf820e574531e261
MikeSmithLab <mikesmithlab@gmail.com>
2022-10-06 18:16:18 +0100

    filters implemented

--- scripts/summaries.js
+++ scripts/summaries.js
@@ -1,22 +1,60 @@
 "use strict";
 
 //DOM elements
-const summaryHeading = Array.from(document.querySelectorAll("h3")).find(
-  (el) => el.textContent === "Research Summaries"
+const filter_buttons = Array.from(document.querySelectorAll("div")).find(
+  (el) => el.id === "myBtnContainer"
 );
+const summaryHeading = Array.from(document.querySelectorAll("div")).find(
+  (el) => el.id === "start"
+);
+const overlayHeading = document.querySelector(".overlay");
 
-const overlayHeading = document.querySelector('.overlay');
+
+//Add filter buttons
+
+//filters.keywords.forEach()
+
+const filterfunction = function(keyword){
+  console.log(keyword);
+}
 
 //Fns add details
-const addSummary = function (publication) 
-{
-  if (publication.summary !== null)
-  {//Don't create summary if text is missing.
+const addButton = function(keyword){
+  filter_buttons.insertAdjacentHTML("afterbegin", `<button class="filterbtn" id=filter${keyword}> ${keyword}</button>`)
+  
+
+}
+
+filter_buttons.addEventListener('click',function(){
+  //remove active from current
+  const current = document.getElementById("myBtnContainer");
+  for (const child of current.children) {
+    child.classList.remove("current");
+  }
+  
+  //Add active to clicked button.
+  const active_btn = document.getElementById(event.target.id);
+  active_btn.className += " current";
+   
+  const summary_blocks = document.getElementsByClassName("summary");
+  [...summary_blocks].forEach(function(block){
+    if (!block.getAttribute('keywords').includes(event.target.id.replace("filter",""))){
+      console.log()
+      block.classList.add("hide-summary");
+    }else{
+      block.classList.remove("hide-summary");
+    }  })
+})
+
+const addSummary = function (publication) {
+  if (publication.summary !== null) {
+    //Don't create summary if text is missing.
     //Summaries without vids
-    if (publication.vid === null) 
-    {summaryHeading.insertAdjacentHTML("afterend",
-      `<a name="${publication.tag}" id=${publication.tag}></a> 
-      <blockquote>
+    if (publication.vid === null) {
+      summaryHeading.insertAdjacentHTML(
+        "afterend",
+        `<a name="${publication.tag}" id=${publication.tag}></a> 
+      <blockquote class="summary" keywords=${publication.keywords}>
       <div>
         <div class="summary-title">
           <h4>${publication.title}</h4>
@@ -32,14 +70,16 @@ const addSummary = function (publication)
         </div>
       </div>
       </blockquote>
-      `);
-    }//Summaries with vids
+      `
+      );
+    } //Summaries with vids
     else {
-        summaryHeading.insertAdjacentHTML("afterend",
+      summaryHeading.insertAdjacentHTML(
+        "afterend",
         `
         <a name="${publication.tag}" id=${publication.tag}></a> 
         
-      <blockquote>
+      <blockquote class="summary" keywords=${publication.keywords}>
       <div>
         <div class="summary-title">
           <h4>${publication.title}</h4>
@@ -57,12 +97,13 @@ const addSummary = function (publication)
           </div>
         </div>
         </blockquote>
-        `);      
-
+        `
+      );
     }
-    if (publication.vid === null){
-    overlayHeading.insertAdjacentHTML('afterend',
-      `<!-- modal window -->
+    if (publication.vid === null) {
+      overlayHeading.insertAdjacentHTML(
+        "afterend",
+        `<!-- modal window -->
       
       <div class="modal hidden" id=${publication.tag}>
         <div class="summary-title">
@@ -82,10 +123,12 @@ const addSummary = function (publication)
       </div>
       
       `
-    )}//full with vids
-    else{
-      overlayHeading.insertAdjacentHTML("afterend",
-      `<!-- modal window -->
+      );
+    } //full with vids
+    else {
+      overlayHeading.insertAdjacentHTML(
+        "afterend",
+        `<!-- modal window -->
       <div class="modal hidden" id=${publication.tag}>
         <div class="summary-title">
         <button class="close-modal">&times;</button>
@@ -110,12 +153,29 @@ const addSummary = function (publication)
             <button class="close-button"">Close</button> 
           </div>
       </div>
-      `); 
-
+      `
+      );
     }
-     
-  };
-}
+  }
+};
+
+
+//Fetch jsons and execute functions
+const xhttp4 = new XMLHttpRequest();
+xhttp4.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    // Typical action to be performed when the document is ready:
+    const response = JSON.parse(xhttp4.responseText);
+    response.keywords.reverse().forEach((keyword) => {
+      addButton(keyword);
+    });
+  }
+};
+
+
+
+
+
 
 const xhttp3 = new XMLHttpRequest();
 xhttp3.onreadystatechange = function () {
@@ -125,9 +185,14 @@ xhttp3.onreadystatechange = function () {
     response.publications.reverse().forEach((publication) => {
       addSummary(publication);
     });
-    
   }
 };
+
+
+
 xhttp3.open("GET", "resources/publications.json", true);
 xhttp3.send();
 
+
+xhttp4.open("GET", "resources/filters.json", true);
+xhttp4.send();
--- scripts/template.js
+++ scripts/template.js
@@ -4,6 +4,17 @@
 const p_tag = Array.from(document.querySelectorAll("p")).find(el => el.className === "thumbs");
 p_tag.insertAdjacentHTML("afterbegin", `<div class="loaded"></div>`);
 
+const thumbnail = document.querySelector(".thumbs");
+thumbnail.addEventListener('click',function(){
+  const summary_blocks = document.getElementsByClassName("summary");
+  [...summary_blocks].forEach(function(block){
+    console.log(block.classList);
+    if (block.classList.contains("hide-summary")){
+      block.classList.remove("hide-summary");
+       }
+      })
+})
+
 //Fns add details
 const addThumb = function (publication, index) {
     p_tag.insertAdjacentHTML("afterbegin", `<a href="researchsummary.html#${publication.tag}"><img src="${publication.img}" alt="${publication.tag}" height="75" width="75" /></a>` 	
